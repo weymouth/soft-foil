6a24650e73b6d60fb7d3851b067357273e33b7d0
Gabe Weymouth <gweymouth@tudelft.nl>
2023-03-03 21:20:57 +0100

    Added motion and better plotting

--- waterlily_example.jl
+++ waterlily_example.jl
@@ -1,33 +1,49 @@
 using WaterLily,Plots,Roots,StaticArrays
 
-function foil(L;Re=38e3)
-	# Define Bezier foil shape
-    P0,P1,P2,P3 = SVector(0,0),SVector(0,0.1),SVector(0.5,0.12),SVector(1.,0.)
+function plot_vorticity(sim)
+    @inside sim.flow.σ[I] = sum(WaterLily.ϕ(i,CartesianIndex(I,i),sim.flow.μ₀) for i ∈ 1:2)/2
+    @inside sim.flow.σ[I] = WaterLily.curl(3, I, sim.flow.u)*sim.flow.σ[I]*sim.L / sim.U
+    contourf(sim.flow.σ',dpi=300,
+			 color=palette(:BuGn), clims=(-10, 10), linewidth=0,
+			 aspect_ratio=:equal, legend=false, border=:none)
+end
+
+function foil(L;d=0,Re=38e3,U=1)
+	# Define symmetric foil shape with cubic Bezier
+    P0,P1,P2,P3 = SA[0,0],SA[0,0.1],SA[.5,0.12],SA[1.,0.]
     A,B,C,D = P0,-3P0+3P1,3P0-6P1+3P2,-P0+3P1-3P2+P3
     curve(t) = A+B*t+C*t^2+D*t^3
     dcurve(t) = B+2C*t+3D*t^2
 
-	# Define distance to foil
+	# Find distance to the symmetric foil
     candidates(X) = union(0,1,find_zeros(t -> (X-curve(t))'*dcurve(t),0,1,naive=true,no_pts=3,xatol=0.01))
     function distance(X,t)
         V = X-curve(t)
-        return copysign(√sum(abs2,V),[V[2],-V[1]]'*dcurve(t))
+        copysign(√sum(abs2,V),[V[2],-V[1]]'*dcurve(t))
     end
-    distance(X) = argmin(abs,distance(X,t) for t in candidates(X))
+    distance(X) = L*argmin(abs, distance(X,t) for t in candidates(X))
+    
+    # Define pitching motion mapping
+    ω = 2π*U/L # reduced frequency k=π
+    center = SA[1,1] # foil placement in domain
+    pivot = SA[.1,0] # pitch location from center
+    function map(x,t)
+        α = 6π/180*cos(ω*t)
+        SA[cos(α) sin(α); -sin(α) cos(α)] * (x/L.-center-pivot) .+ pivot
+    end
 
-	# Define body and simulation
-	body = AutoBody((x,t) -> distance(SVector(x[1]/L-1,abs(x[2]/L-0.5))))
-    return Simulation((4L+2,L+2),[1,0],L;ν=L/Re,body)
+    # Define sdf to deflected foil and Simulation
+    deflect(x) = max(0,x-0.3)^2/0.7^2
+    sdf(x,t) = distance(SA[x[1],abs(x[2]+d*deflect(x[1]))])
+    Simulation((4L+2,2L+2),[U,0],L;ν=U*L/Re,body=AutoBody(sdf,map))
 end
 
-function plot_vorticity(sim)
-	@inside sim.flow.σ[I] = WaterLily.curl(3, I, sim.flow.u) * sim.L / sim.U
-	contourf(sim.flow.σ',
-			 color=palette(:BuGn), clims=(-5, 5), linewidth=0,
-			 aspect_ratio=:equal, legend=false, border=:none)
-end
-
-sim = foil(64);
-sim_step!(sim,10);
+sim = foil(256,d=0.12);
+sim_step!(sim,4,remeasure=true);
 plot_vorticity(sim)
-savefig("flow.png")
+savefig("flow_d_12.png")
+
+@gif for t ∈ 4:1/24:(6-1/24)
+    sim_step!(sim,t,remeasure=true)
+    plot_vorticity(sim)
+end
